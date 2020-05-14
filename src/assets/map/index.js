2b597912f582e2506d24f86cea1615faba91483c
wangtanke <1665497618@qq.com>
2020-05-15 00:27:30 +0800

    gaodeditu

--- src/assets/map/index.js
+++ src/assets/map/index.js
@@ -1,22 +1,62 @@
 class Map {
-  constructor(){
+  constructor() {
     this.config = {
-      url:"http://www.nylv.xyz:4000/4.15/init.js",
-      css:"http://www.nylv.xyz:4000/4.15/esri/themes/light/main.css"
+      url: "http://www.nylv.xyz:4000/4.15/init.js",
+      css: "http://www.nylv.xyz:4000/4.15/esri/themes/light/main.css"
     }
     this.map = null
     this.view = null
+    this.gaodemap = null
   }
   //将map和view加入Map实例
-  initMap(options){
-    this.view = options.view||null
+  initMap(options) {
+    this.view = options.view || null
     this.map = options.map || null
   }
-  static function test(){
+  static test() {
     //静态方法 无法调用this中的属性
   }
-  test(){
+  test() {
     //动态方法 可以调用this
+  }
+
+  createMap(BaseTileLayer,esriRequest,ArcGISMap) {
+
+    var GaodeLayer = BaseTileLayer.createSubclass({
+      properties: {
+        urlTemplate: null,
+      },
+      getTileUrl: function (level, row, col) {
+        return  'http://webrd0' + (col % 4 + 1) + '.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x=' + col + '&y=' + row + '&z=' + level;
+      },
+      fetchTile: function (level, row, col) {
+        var url = this.getTileUrl(level, row, col);
+        return esriRequest(url, {
+            responseType: "image"
+          })
+          .then(function (response) {
+
+            var image = response.data;
+            var width = this.tileInfo.size[0];
+            var height = this.tileInfo.size[0];
+
+            var canvas = document.createElement("canvas");
+            var context = canvas.getContext("2d");
+            canvas.width = width;
+            canvas.height = height;
+
+            context.drawImage(image, 0, 0, width, height);
+
+            return canvas;
+          }.bind(this));
+      }
+    })
+    //新建矢量图层
+    var gaodeLayer = new GaodeLayer();
+
+    this.gaodemap = new ArcGISMap({
+      layers: [gaodeLayer]
+    });
   }
 }
 
--- src/components/map/map.vue
+++ src/components/map/map.vue
@@ -12,23 +12,34 @@ export default {
     const esrimap = new esriMap();
     // lazy load the required ArcGIS API for JavaScript modules and CSS
     loadModules(
-      ["esri/Map", "esri/views/MapView"],
+      ["esri/Map", "esri/views/MapView", "esri/layers/BaseTileLayer",  "esri/request"],
       { css: true },
       {
         url: esrimap.config.url,
         css: esrimap.config.css
       }
-    ).then(([ArcGISMap, MapView]) => {
-      const map = new ArcGISMap({
-        basemap: "topo-vector"
-      });
+    ).then(([ArcGISMap, MapView, BaseTileLayer, esriRequest]) => {
+
+      // const map = new ArcGISMap({
+      //   basemap: "topo-vector"
+      // });
+
+      var newmap = new esriMap()
+      newmap.createMap(BaseTileLayer,esriRequest,ArcGISMap)
 
       this.view = new MapView({
         container: this.$el,
-        map: map,
-        center: [-118, 34],
-        zoom: 8
+        map: newmap.gaodemap,
+        center: [103.0419678465576,30.01008405698828],
+        zoom: 12,                 //min 3   max 18
+        constraints:{
+          minZoom:3,
+          maxZoom:18
+        }
       });
+
+      this.view.ui.remove("attribution")  // 去掉右下角 powed by Esri 字样
+      console.log(this.view)
     });
   },
   beforeDestroy() {
@@ -36,7 +47,7 @@ export default {
       // destroy the map view
       this.view.container = null;
     }
-  }
+  },
 };
 </script>
 
